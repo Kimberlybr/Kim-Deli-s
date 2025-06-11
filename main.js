6a8131dc7f37248d93a2741c832a4c5451e32648
Kimberlybr <kimberlyfernanda.barcenas.cb10@gmail.com>
2025-06-11 10:24:58 -0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-window.onload = fuction () {
-var reply = prompt ("Bienvenid@s  ¿cuál es tu nombre?", "")
-alert ("Es un gusto tenerte aqui" + reply "!")
-}
-let miBoton =document.querySelector('button');
-miBoton.onclick = fuction () {
-alert ('Con el código:kb18 obtendrás un 10% en tu próxima compra');
-}
